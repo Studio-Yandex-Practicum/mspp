b8f95c3dce730b42c08dd658dc4543d23a1729b8
Serge Balyaba <erges699@gmail.com>
2023-01-27 09:42:03 +0300

    20230127-01 Create models.py

--- apps/bot/models.py
+++ apps/bot/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+class City(models.Model):
+    name = models.CharField(
+        verbose_name='Наименование',
+        max_length=100,
+        help_text='Название города.',
+        )
+    
+    class Meta:
+        ordering = ('name', )
+        verbose_name = 'Город'
+        verbose_name_plural = 'Города'
+    
+    def __str__(self):
+        return self.name
+
+
+class Fund(models.Model):
+    name = models.CharField(
+        verbose_name='Наименование',
+        max_length=256,
+        help_text='Название фонда.',
+        )
+        
+    def __str__(self):
+        return self.name
+
+
+class Limitation(models.Model):
+    from_age = models.PositiveSmallIntegerField(
+        verbose_name='Возраст',
+        )
+    to_age = models.PositiveSmallIntegerField(
+        verbose_name='Возраст',
+        )
+        
+    def __str__(self):
+        return f'От {self.from_age} до {self.to_age}'
