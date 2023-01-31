912a532f0a20d4dd61aa5e530e5437efd20a8eb2
Vladislav <ivanyuk.vl@yandex.ru>
2023-01-31 22:55:20 +1000

    Move bot settings to project settings

--- src/apps/bot/bot.py
+++ src/apps/bot/bot.py
@@ -1,16 +1,19 @@
+from urllib.parse import urljoin
+
 from telegram.ext import Application
 
-from .handlers import HANDLERS
-from .settings import TELEGRAM_TOKEN, WEBHOOK_MODE, WEBHOOK_URL
+import config.settings as settings
 
-bot_app = Application.builder().token(TELEGRAM_TOKEN).build()
+from .handlers import HANDLERS
+
+bot_app = Application.builder().token(settings.TELEGRAM_TOKEN).build()
 bot_app.add_handlers(HANDLERS)
 
 
 async def start_bot():
     await bot_app.initialize()
-    if WEBHOOK_MODE:
-        await bot_app.bot.set_webhook(WEBHOOK_URL)
+    if settings.WEBHOOK_MODE:
+        await bot_app.bot.set_webhook(urljoin(settings.WEBHOOK_URL, "bot/"))
     else:
         await bot_app.updater.start_polling()
     await bot_app.start()
--- src/apps/bot/settings.py
+++
@@ -1,5 +0,0 @@
-import os
-
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-WEBHOOK_MODE = os.getenv("WEBHOOK_MODE", "False").lower() == "true"
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")
