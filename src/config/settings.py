f052962a669fe7b3536a30646fce9e3aad00da64
Vladislav <ivanyuk.vl@yandex.ru>
2023-02-08 01:25:15 +1000

    Fix database folder

--- src/config/settings.py
+++ src/config/settings.py
@@ -69,10 +69,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "config.wsgi.application"
 
+
+DB_DIR = BASE_DIR / ".data"
+DB_DIR.mkdir(exist_ok=True)
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / ".data/db.sqlite3",
+        "NAME": DB_DIR / "db.sqlite3",
     }
 }
 
