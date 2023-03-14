f47ac12b88caa823dbe8702422544924a8b01dd9
Alexander Sosnovskiy <alansosnovsky@yandex.ru>
2023-03-14 15:21:51 +0300

    Added function. Replaced handler

--- src/apps/bot/logger.py
+++ src/apps/bot/logger.py
@@ -1,11 +1,18 @@
+import os
 import logging
+from logging.handlers import TimedRotatingFileHandler
 
 from django.conf import settings
 
+def create_log_directory(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+create_log_directory(settings.LOG_DIR)
 logger = logging.getLogger(__name__)
 logger.setLevel(settings.LOGGING_LEVEL)
 
-filelog = logging.FileHandler(settings.LOGGING_FILENAME_BOT)
+filelog = TimedRotatingFileHandler(settings.LOGGING_FILENAME_BOT)
 filelog.setLevel(settings.LOGGING_LEVEL)
 
 console = logging.StreamHandler()
