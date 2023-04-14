a707a150e9bac7c5e91eb05a48629e51483170b8
Lenovo <vlad352020@gmail.com>
2023-04-14 03:00:33 +0300

    add ssl header

--- src/config/settings.py
+++ src/config/settings.py
@@ -19,6 +19,8 @@ CSRF_TRUSTED_ORIGINS = list(
     map(str.strip, env.list("CSRF_TRUSTED_ORIGINS", default=["http://127.0.0.1", "http://localhost"]))
 )
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
